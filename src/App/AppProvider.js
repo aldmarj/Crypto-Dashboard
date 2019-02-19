eea6693644ec4cdae7c0e867f8e2b61256cb43cc
Aldmar <aldmar.joubert@students.plymouth.ac.uk>
2019-02-19 13:54:30 +0000

    Finished add and remove coins

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,18 +1,41 @@
 import React from 'react';
+import _ from 'lodash';
 
 const cc = require('cryptocompare');
 
 export const AppContext = React.createContext();
+
+const MAX_FAVOURITES = 10;
 
 export class AppProvider extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             page: 'dashboard',
+            favourites: ['BTC', 'ETH', 'XMR', 'XRP'],
             ...this.savedSettings(),
             setPage: this.setPage,
+            addCoin: this.addCoin,
+            removeCoin: this.removeCoin,
+            isInFavourites: this.isInFavourites,
             confirmFavourites: this.confirmFavourites
         }
+    }
+
+    addCoin = key => {
+        let favourites = [...this.state.favourites];
+        if(favourites.length < MAX_FAVOURITES){
+            favourites.push(key);
+            this.setState({favourites});
+        }
+    }
+
+    isInFavourites = key => _.includes(this.state.favourites, key)
+
+    removeCoin = key => {
+        let favourites = [...this.state.favourites];
+
+        this.setState({favourites: _.pull(favourites, key)});
     }
 
     componentDidMount = () => {
@@ -30,7 +53,7 @@ export class AppProvider extends React.Component {
             page: 'dashboard'
         });
         localStorage.setItem('cryptoDash', JSON.stringify({
-            test: 'hello'
+            favourites: this.state.favourites
         }));
     }
 
@@ -39,7 +62,8 @@ export class AppProvider extends React.Component {
         if(!cryptoDashData){
             return {page: 'settings', firstVisit: true }
         }
-        return {};
+        let {favourites} = cryptoDashData;
+        return {favourites};
     }
 
     setPage = page => this.setState({page})
--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -9,15 +9,15 @@ export const CoinGridStyled = styled.div`
     grid-gap: 15px;
     margin-top: 40px;
 `
-function getCoinsToDisplay(coinList, topSection){
-    return Object.keys(coinList).slice(0, topSection ? 10 : 100);
+function getCoinsToDisplay(coinList, topSection, favourites){
+    return topSection ? favourites : Object.keys(coinList).slice(0, 100);
 }
 
 export default function ({topSection}){
     return (
         <AppContext.Consumer>
-            {({coinList}) => <CoinGridStyled>
-                {getCoinsToDisplay(coinList, topSection).map(coinKey =>
+            {({coinList, favourites}) => <CoinGridStyled>
+                {getCoinsToDisplay(coinList, topSection, favourites).map(coinKey =>
                     <CoinTile topSection={topSection} coinKey={coinKey} />                    )}
             </CoinGridStyled>}
         </AppContext.Consumer>
